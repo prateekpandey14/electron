8715eb70130099b41d1f186c268a351b22fe887f
Prateek <prateekpandey14@gmail.com>
2017-11-30 14:08:28 +0530

    Add watcher and client

---
+++ client.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2017 The OpenEBS Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Some of the code below came from https://github.com/coreos/etcd-operator
+which also has the apache 2.0 license.
+*/
+
+// Package kit for Kubernetes operators
+package kubeoperator
+
+import (
+	"github.com/coreos/pkg/capnslog"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/rest"
+)
+
+const (
+	serverVersionV170 = "v0.1.0"
+)
+
+var logger = capnslog.NewPackageLogger("github.com/prateekpaney14/kube-operator", "OpenEBS")
+
+// NewHTTPClient creates a Kubernetes client to interact with API extensions for Custom Resources
+func NewHTTPClient(group, version string, schemeBuilder runtime.SchemeBuilder) (*rest.RESTClient, *runtime.Scheme, error) {
+	scheme := runtime.NewScheme()
+	if err := schemeBuilder.AddToScheme(scheme); err != nil {
+		return nil, nil, err
+	}
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	config.GroupVersion = &schema.GroupVersion{Group: group, Version: version}
+
+	config.APIPath = "/apis"
+	config.ContentType = runtime.ContentTypeJSON
+	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: serializer.NewCodecFactory(scheme)}
+
+	client, err := rest.RESTClientFor(config)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return client, scheme, nil
+}
